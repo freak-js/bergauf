8c6b360aebbc4bb430401300ec215fad771f7726
freak-js <mahrov.web@gmail.com>
2019-12-14 17:12:33 +0300

    Добавлена страница отображения списка дистрибьюторов.

--- autoxl/views.py
+++ autoxl/views.py
@@ -14,31 +14,36 @@ def distributor(request):
     return render(request, 'autoxl/distributor.html')
 
 
+def distributors(request):
+    distributors = Distributor.objects.all().filter(active=True)
+    return render(request, 'autoxl/distributors.html', {'distributors': distributors})
+
 @require_POST
 def save_distributor(request):
     distributor = Distributor.save_distributor(request)
     return redirect('distributors') if distributor else redirect('notice', {'context': 'Произошла ошибка'})
 
 
-def go(request):
-    work_sheet = get_work_sheet(request)
-    bonus_count = int(request.POST.get('number'))
-
-    if work_sheet:
-        report = generate_report(work_sheet, bonus_count)
-
-        if isinstance(report, list):
-            return render(request, 'autoxl/notice.html', {'errors': report})
-
-        response = HttpResponse(
-            save_virtual_workbook(report),
-            content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-        )
-        response['Content-Disposition'] = 'attachment; filename=report.xlsx'
-        return response
-    return render(request, 'autoxl/notice.html', {'context': 'Произошла ошибка'})
-
-
 def notice(request):
     return render(request, 'autoxl/notice.html')
 
+
+
+
+# def go(request):
+#     work_sheet = get_work_sheet(request)
+#     bonus_count = int(request.POST.get('number'))
+#
+#     if work_sheet:
+#         report = generate_report(work_sheet, bonus_count)
+#
+#         if isinstance(report, list):
+#             return render(request, 'autoxl/notice.html', {'errors': report})
+#
+#         response = HttpResponse(
+#             save_virtual_workbook(report),
+#             content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+#         )
+#         response['Content-Disposition'] = 'attachment; filename=report.xlsx'
+#         return response
+#     return render(request, 'autoxl/notice.html', {'context': 'Произошла ошибка'})
