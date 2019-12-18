9a2248633478701d3ceafa6d64ec8f8433ea695d
freak-js <mahrov.web@gmail.com>
2019-12-18 11:41:40 +0300

    Добавлены комментарии.

--- autoxl/utils/utils.py
+++ autoxl/utils/utils.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
 
+'''
+Модуль вспомогательных утилит
+'''
+
 def redirect_to_error_page(request, context=''):
+    '''
+    redirect_to_error_page - функция обработки контекста ошибок.
+
+    Parameters
+    ----------
+    request - объект запрса Django принимаемый во views.
+    context - текстовое описание ошибки передаваемой на экран notice.
+
+    Returns
+    -------
+    None, рендерит экран уведомдлений.
+    '''
     return render(request, 'autoxl/notice.html', {'context': f'Произошла ошибка {context}'})
