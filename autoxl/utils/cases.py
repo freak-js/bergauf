104ff1e67a13699ade51a363ed314df853ac4571
freak-js <mahrov.web@gmail.com>
2019-12-23 03:46:08 +0300

    Выполнена форма входа в систему с редиректами и прочим.

--- autoxl/utils/cases.py
+++ autoxl/utils/cases.py
@@ -13,8 +13,7 @@ class BaseOneFileCabinet:
     def __init__(self, file, bonus_count):
         self.bonus_count = bonus_count
         self.work_book = openpyxl.load_workbook(file)
-        self.first_sheet = self.work_book.get_sheet_names()[0]
-        self.work_sheet = self.work_book.get_sheet_by_name(self.first_sheet)
+        self.work_sheet = self.work_book.get_sheet_by_name(self.work_book.get_sheet_names()[0])
         self.data = self.get_managers_data()
 
 
