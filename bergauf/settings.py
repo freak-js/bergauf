fed7c5eefd16adcb788a4d3f3d6c1385e99c1cbb
freak-js <mahrov.web@gmail.com>
2020-01-12 19:16:04 +0300

    Подготовка к деплою.

--- bergauf/settings.py
+++ bergauf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#wot+5)jyk&**(t3lhn3eauatm1zlbs6y)0))%pc92le8t6w14'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['45.128.204.29']
 
 
 # Application definition
